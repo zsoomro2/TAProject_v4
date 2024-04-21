17b26dc73b5dc0bb36515c7301ee13264836a72b
jvuko <jvuko147@gmail.com>
2024-04-21 09:39:20 -0500

    -Added test cases/getters for login class

--- Unit_Tests/LoginTests.py
+++ Unit_Tests/LoginTests.py
@@ -5,15 +5,26 @@ from classes import Login
 
 class LoginTests(unittest.TestCase):
     def setUp(self):
-        User.objects.create_user(username='test', password='<PASSWORD>')
+        User.objects.create(username='test', password='<PASSWORD>', fname='test', lname='test_lname', role='TA')
 
     def test_getUsername(self):
-        self.assertEquals(User.objects.getUsername('test'), 'test')
+        self.assertEquals(Login.getUsername('test'), 'test')
 
     def test_badUsername(self):
-        try:
-            username = User.objects.getUsername('')
-        except Exception as e:
-            with self.assertRaises(e, msg="User not found"):
-                username = User.objects.getUsername('')
+        username = Login.getUsername('badUser')
+        self.assertEquals(username, None, msg="Username not found")
+
+    def test_getPassword(self):
+        self.assertEquals(Login.getPassword('<PASSWORD>'), '<PASSWORD>')
+
+    def test_badPassword(self):
+        self.assertEquals(Login.getPassword('badPassword'), '<PASSWORD>', msg="Password not found")
+
+    def test_getRole(self):
+        self.assertEquals(Login.getRole('TA'), 'TA')
+
+    def test_badRole(self):
+        self.assertEquals(Login.getRole('badRole'), None, msg="Role not found")
+
+
 
--- classes/Login.py
+++ classes/Login.py
@@ -1,13 +1,16 @@
 from ta_app.models import User
 
 class Login:
-    def __init__(self, username):
-        pass
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = password
+        self.role = role
 
     def getUsername(self):
-        pass
-    def getPassword(self):
-        pass
+        return self.username
 
-    def findUser(self):
-        pass
+    def getPassword(self):
+        return self.password
+
+    def getRole(self):
+        return self.role
