87d47db38c9b80f2e66502903eaad8919d339de9
quinn-schmidt <144085933+quinn-schmidt@users.noreply.github.com>
2024-04-21 00:18:21 -0500

    Update CourseClass.py

--- classes/CourseClass.py
+++ classes/CourseClass.py
@@ -1,9 +1,30 @@
 class Course:
-    def __init__(self, course_name, section):
-        pass
-
-    def update_course(self, start=None, end=None, credit=None):
-        pass
-
-    def get_details(self):
-        pass
+    def __init__(self, name, section, start, end, credits, instructor, ta):
+        self.name = name
+        self.section = section
+        self.start = start
+        self.end = end
+        self.credits = credits
+        self.instructor = instructor
+        self.ta = ta
+    
+    def get_name(self):
+        return self.name
+        
+    def get_section(self):
+        return self.section
+        
+    def get_start(self):
+        return self.start
+        
+    def get_end(self):
+        return self.end
+        
+    def get_credits(self):
+        return self.credits
+        
+    def get_instructor(self):
+        return self.instructor
+        
+    def get_ta(self):
+        return self.ta
