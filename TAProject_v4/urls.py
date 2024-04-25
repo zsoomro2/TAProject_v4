cd1f329d8b7230dccd1b8eb17dfdef088770e125
jvuko <jvuko147@gmail.com>
2024-04-24 22:18:38 -0500

    - created a css for forms - updated add_user.html, Adduser logic, - added AddUser to navbar, added tables to supervisor.html - +1 unittest, +1 acceptance test, updated views, added an edit page doesnt do anything but is there

--- TAProject_v4/urls.py
+++ TAProject_v4/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import edit
-
 from ta_app.views import login, supervisor, instructor, ta, adduser, LogoutView, edit
 
 urlpatterns = [
