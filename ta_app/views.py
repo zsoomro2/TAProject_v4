a9a76e9f404eb20f414d52c3da7cb6f554a5e726
zsoomro2 <zsoomro@uwm.edu>
2024-05-10 03:27:39 -0500

    Permissions working

--- ta_app/views.py
+++ ta_app/views.py
@@ -8,7 +8,11 @@ from classes.User import MyUser
 from django.http import HttpResponseRedirect
 
 
-# Create your views here.
+# Utility function to redirect based on role
+def redirect_to_role_home(request):
+    role = request.session.get('role', 'login').lower()
+    return redirect(role)
+
 
 class login(View):
 
@@ -28,28 +32,26 @@ class login(View):
             request.session.set_expiry(300)  # Session expires in 5 minutes
             request.session.modified = True
 
-            if user_obj.role == "Supervisor":
-                return redirect('supervisor')
-            elif user_obj.role == "Instructor":
-                return redirect('instructor')
-            elif user_obj.role == "TA":
-                return redirect('ta')
+            return redirect_to_role_home(request)
         else:
             return render(request, 'login.html', {'message': "Login failed. Please try again."})
 
 
 class adduser(View):
     def get(self, request):
-        users = User.objects.all()  # Fetch all user objects
+        if request.session.get('role') != 'Supervisor':
+            return redirect_to_role_home(request)
+        users = User.objects.all()
         return render(request, "add_user.html", {'role_choices': Roles.choices, 'users': users})
 
     def post(self, request):
+        if request.session.get('role') != 'Supervisor':
+            return redirect_to_role_home(request)
         new_user = AddUser(request.POST['username'], request.POST['password'], request.POST['fname'],
                            request.POST['lname'], request.POST['role'])
         isvalid = new_user.validate()
         check = new_user.checkUser()
 
-        # Initialize the context with role_choices to always include them in the rendering
         context = {'role_choices': Roles.choices}
 
         if isvalid and not check:
@@ -61,7 +63,6 @@ class adduser(View):
         elif not isvalid:
             context['message'] = "There was an error validating the form"
 
-        # Fetch all users regardless of the outcome to display the list of users
         context['user_list'] = User.objects.all()
         context['course_list'] = Course.objects.all()
         context['section_list'] = Section.objects.all()
@@ -70,31 +71,39 @@ class adduser(View):
 
 class supervisor(View):
     def get(self, request):
+        if request.session.get('role') != 'Supervisor':
+            return redirect_to_role_home(request)
         user_list = User.objects.all()
         course_list = Course.objects.all()
         section_list = Section.objects.all()
         user_id = request.session.get('user_id')
 
         current_user = User.objects.get(id=user_id)
-        return render(request, 'supervisor.html', {'user_list': user_list, 'course_list': course_list
-            , 'section_list': section_list,'current_user': current_user})
+        return render(request, 'supervisor.html', {'user_list': user_list, 'course_list': course_list,
+                                                   'section_list': section_list, 'current_user': current_user})
 
 
 class user_page(View):
     def get(self, request):
+        if request.session.get('role') != 'Supervisor':
+            return redirect_to_role_home(request)
         user_list = User.objects.all()
         return render(request, 'editUser.html', {'user_list': user_list})
 
 
 class instructor(View):
     def get(self, request):
-        users = User.objects.all()  # Fetch all user objects
+        if request.session.get('role') != 'Instructor':
+            return redirect_to_role_home(request)
+        users = User.objects.all()
         return render(request, "instructor.html", {'users': users})
 
 
 class ta(View):
     def get(self, request):
-        return render(request, "supervisor.html", {})
+        if request.session.get('role') != 'TA':
+            return redirect_to_role_home(request)
+        return render(request, "ta.html", {})
 
 
 class LogoutView(View):
@@ -102,21 +111,23 @@ class LogoutView(View):
         session_keys = list(request.session.keys())
         for key in session_keys:
             del request.session[key]
-        return HttpResponseRedirect('/')  # Redirect to the homepage or login page
+        return HttpResponseRedirect('/')
 
 
 class edit(View):
     def get(self, request, username):
         thing = EditClass(request, username)
         if thing.isUser():
+            if request.session.get('role') != 'Supervisor':
+                return redirect_to_role_home(request)
             user = User.objects.get(username=username)
             return render(request, 'edit.html', {'username': user, 'role_choices': Roles.choices,
                                                  'isUser': thing.isUser()})
 
         elif thing.isCourse():
+            if request.session.get('role') != 'Supervisor':
+                return redirect_to_role_home(request)
             course = Course.objects.get(Course_name=username)
-            # ta_list = User.objects.filter(role='TA')
-            # instructor_list = User.objects.filter(role='Instructor')
             context = {'username': course, 'isCourse': thing.isCourse(), 'MeetType': MeetType.choices}
             return render(request, 'edit.html', context)
 
@@ -130,9 +141,13 @@ class edit(View):
         context = {}
 
         if isUser:
+            if request.session.get('role') != 'Supervisor':
+                return redirect_to_role_home(request)
             update = thing.updateUser(request, username)
 
         elif isCourse:
+            if request.session.get('role') != 'Supervisor':
+                return redirect_to_role_home(request)
             update = thing.updateCourse(request, username)
 
         if update:
@@ -157,9 +172,13 @@ class edit(View):
 
 class Delete(View):
     def get(self, request, username):
+        if request.session.get('role') != 'Supervisor':
+            return redirect_to_role_home(request)
         return render(request, 'delete.html', {'username': username})
 
     def post(self, request, username):
+        if request.session.get('role') != 'Supervisor':
+            return redirect_to_role_home(request)
         thing = EditClass(request, username)
         if thing.isUser():
             user = User.objects.get(username=username)
@@ -183,6 +202,8 @@ class Delete(View):
 
 class ViewCourse(View):
     def get(self, request, course_name):
+        if request.session.get('role') not in ['Supervisor', 'Instructor']:
+            return redirect_to_role_home(request)
         course = Course.objects.filter(Course_name=course_name)
         section_list = Section.objects.all()
         return render(request, 'viewcourse.html', {'course_list': course,
@@ -191,6 +212,8 @@ class ViewCourse(View):
 
 class viewAssignments(View):
     def get(self, request):
+        if request.session.get('role') != 'Supervisor':
+            return redirect_to_role_home(request)
         users = User.objects.all()
         assignments = []
 
